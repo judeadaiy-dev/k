f9d2d81247217026b4ec9acb89d11c1fe502394c
judeadaiy-dev <judeadaiy@gmail.com>
2026-05-14 09:18:46 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        sea: {
+          900: '#0A1929', 800: '#0F2A43', 500: '#38BDF8', 400: '#22D3EE'
+        }
+      }
+    }
+  },
+  plugins: [],
+}
