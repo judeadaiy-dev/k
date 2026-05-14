5454f923420e6474891b83626a57e787feff4840
judeadaiy-dev <judeadaiy@gmail.com>
2026-05-14 11:51:18 +0300

    Create welcome.js

---
+++ welcome.js
@@ -0,0 +1,5 @@
+<Link href="/(auth)/signup" asChild>
+  <TouchableOpacity style={[styles.button, {backgroundColor: '#10B981', marginTop: 10}]}>
+    <Text style={styles.buttonText}>حساب جديد</Text>
+  </TouchableOpacity>
+</Link>
